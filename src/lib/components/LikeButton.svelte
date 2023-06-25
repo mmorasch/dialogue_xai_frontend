c9f2b188afef9e8e0ac134bde03840813ef958cc
Michael Morasch <mmorasch@posteo.de>
2023-06-25 17:32:12 +0200

    wip

--- src/lib/components/LikeButton.svelte
+++ src/lib/components/LikeButton.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import like from '$lib/assets/like.png';
-	import backend from '$lib/backend';
+	import Modal from './Modal.svelte';
+
+	let showModal = true;
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<img class="mr-2.5 mt-2" src={like} alt="" width="20" height="20" on:click={backend.firebase.positive_feedback}/>
+<img class="mr-2.5 mt-2" src={like} alt="" width="20" height="20" on:click={() => showModal = !showModal}/>
+<Modal type={"positive"} showModal={showModal} />
--- src/lib/components/TTM-Chat.svelte
+++ src/lib/components/TTM-Chat.svelte
@@ -1,17 +1,36 @@
 <script lang="ts">
 	import type { TChatMessage } from '$lib/types';
+	import { afterUpdate, tick } from 'svelte';
 	import Header from './Header.svelte';
 	import Message from './Message.svelte';
 
 	export let messages: TChatMessage[] = [];
+	let element: HTMLElement;
+
+	// Either afterUpdate()
+	afterUpdate(() => {
+		console.log('afterUpdate');
+		if (messages) scrollToBottom(element);
+	});
+
+	$: if (messages && element) {
+		console.log('tick');
+		scrollToBottom(element);
+	}
+
+	const scrollToBottom = async (node: HTMLElement) => {
+		node.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+	};
 </script>
 
-<div class="col-[2] flex-1 overflow-y-auto h-[calc(100%_-_10px)]">
-	<div class="ttm flex justify-between w-full h-[calc(100%_-_10px)] border-[length:var(--border)] shadow-[0_15px_15px_-5px_rgba(0,0,0,0.2)] rounded-[5px]">
+<div bind:this={element} class="col-[2] flex-1 overflow-y-auto h-full">
+	<div
+		class="ttm flex justify-between w-full h-full border-[length:var(--border)] shadow-[0_15px_15px_-5px_rgba(0,0,0,0.2)] rounded-[5px]"
+	>
 		<Header>Chat Window</Header>
-		<main class="col-[2] flex-1 overflow-y-auto h-[calc(100%_-_10px)] p-2.5">
+		<main class="col-[2] flex-1 overflow-y-auto h-full p-2.5">
 			{#each messages as message}
-				<Message {message}/>
+				<Message {message} />
 			{/each}
 		</main>
 	</div>
