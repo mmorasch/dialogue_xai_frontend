dbd502014dad793cf3dc37d9947b80bd69a162bd
Michael Morasch <mmorasch@posteo.de>
2023-09-14 14:31:11 +0200

    wip

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM node:20-alpine3.17
 RUN npm install -g pnpm
 ADD . /app
 WORKDIR /app
-RUN rm -rf node_modules
 RUN pnpm install
 RUN pnpm run build
 EXPOSE 3000
--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -1,4 +1,5 @@
 export default {
-  BACKEND: 'https://webtentacle1.techfak.uni-bielefeld.de/dialogue-api/',
+  // BACKEND: 'https://webtentacle1.techfak.uni-bielefeld.de/dialogue-api/',
+  BACKEND: 'http://192.168.178.63:4455/',
   BACKEND_LOCAL: 'http://192.168.178.63:4455/'
 }
