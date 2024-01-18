80364095539c87fd78d0d296e4b47936990b31f8
dimi9595 <dmitry.mindlin@gmail.com>
2024-01-18 10:49:26 +0100

    Added TooltipIcon.svelte

---
+++ src/lib/components/TooltipIcon.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  export let message = '';
+</script>
+
+<span class="tooltip-icon-container">
+  <span class="tooltip-icon">?</span>
+  <span class="tooltip-text">{message}</span>
+</span>
+
+<style>
+  .tooltip-icon-container {
+    position: relative;
+    display: inline-block;
+  }
+.tooltip-icon {
+  background-color: grey; /* Change color to grey */
+  color: #fff;
+  text-align: center;
+  border-radius: 50%;
+  width: 16px; /* Make it smaller */
+  height: 16px; /* Make it smaller */
+  display: inline-block;
+  line-height: 16px; /* Make it smaller */
+  cursor: help;
+}
+  .tooltip-text {
+    visibility: hidden;
+    width: 120px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    border-radius: 6px;
+    padding: 5px 0;
+    position: absolute;
+    z-index: 1;
+    bottom: 125%;
+    left: 50%;
+    margin-left: -60px;
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+  .tooltip-icon-container:hover .tooltip-text {
+    visibility: visible;
+    opacity: 1;
+  }
+</style>
