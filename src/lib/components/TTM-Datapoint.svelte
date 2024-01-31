3a6a3b842129b342a34edf1342db870f1cfe2389
dimi9595 <dmitry.mindlin@gmail.com>
2024-01-31 14:54:08 +0100

    Added final testing.

--- src/lib/components/TTM-Datapoint.svelte
+++ src/lib/components/TTM-Datapoint.svelte
@@ -1,42 +1,75 @@
 <script lang="ts">
-    import {PUBLIC_TEACH_TEST_CYCLES} from '$env/static/public';
+    import {PUBLIC_TEACH_TEST_CYCLES, PUBLIC_END_TEST_CYCLES} from '$env/static/public';
     import {ListBox, ListBoxItem} from '@skeletonlabs/skeleton';
     import Header from './Header.svelte';
     import Datapoint from './Datapoint.svelte';
-    import type {TTestOrTeaching} from '$lib/types';
+    import type {TInteractiveOrStatic, TTestOrTeaching} from '$lib/types';
+    import backend, {logEvent, logTestingResponse} from '../backend.ts';
 
     export let testOrTeaching: TTestOrTeaching;
+    export let interactiveOrStatic: TInteractiveOrStatic;
     export let feature_names;
+
+    export let true_label: string;
 
     export let data: { [key: string]: string };
     export let feature_tooltips: { [key: string]: string };
     export let feature_units: { [key: string]: string };
     export let prediction_question =
-        'Will the model predict high or low diabetes risk for the individual?';
-    export let teaching_intro =
-        "Provide an initial guess for the patient and ask questions to understand the model's prediction.";
+        'Will the model assign a likely or unlikely Risk for diabetes for the patient?';
+
+    export let teaching_intro = "";
+    if (interactiveOrStatic === 'static') {
+        teaching_intro = "Guess the patients risk and read the explanations to understand the prediction.";
+    } else {
+        teaching_intro = "Guess the patients risk and ask questions to understand the model's prediction.";
+    }
+
     export let test_intro =
-        "Test your knowledge. Based on what you learned before, what do you think the model will predict for this patient? This time you cannot ask questions.";
+        "Test your knowledge about the model. Based on what you learned before, what do you think the model will predict for this patient? This time you cannot ask questions.";
 
     export let datapoint_count: number | null = null;
 
+    export let user_id: string | null = null;
 
     const options = [
-        'Surely High Risk',
-        'Rather High Risk',
+        'Surely Likely',
+        'Rather Likely',
         "I don't know",
-        'Rather Low Risk',
-        'Surely Low Risk'
+        'Rather Unlikely',
+        'Surely Unlikely'
     ];
 
     // get event categories
     export let selected_prediction: string | null = null;
+
+    let isDisabled = false;
+
+    function logPredictionAndDisable(userid: string,
+                                     option: string,
+                                     datapointCount: number,
+                                     true_label: string) {
+        if (!isDisabled) {
+            if (testOrTeaching === 'test') {
+                logTestingResponse(userid, datapointCount, option, false, true_label);
+            } else if (testOrTeaching === 'teaching') {
+                logEvent(userid, 'TTM-Datapoint', 'user_prediction', datapointCount, {
+                    prediction: option,
+                    true_label: true_label
+                });
+                isDisabled = true;
+            } else {
+                logTestingResponse(userid, datapointCount, option, true, true_label);
+            }
+        }
+    }
 </script>
 
 
 <div
         class:teaching-color={testOrTeaching === 'teaching'}
         class:testing-color={testOrTeaching === 'test'}
+        class:final-testing-color={testOrTeaching === 'final-test'}
         class="inputarea h-full flex-1 overflow-y-auto shadow-[0_15px_15px_-5px_rgba(0,0,0,0.2)] mx-1.5 my-0"
 >
     {#if testOrTeaching === 'test'}
@@ -45,13 +78,16 @@
     {#if testOrTeaching === 'teaching'}
         <Header>Patient {datapoint_count} of {PUBLIC_TEACH_TEST_CYCLES}</Header>
     {/if}
+    {#if testOrTeaching === 'final-test'}
+        <Header>Patient {datapoint_count} of {PUBLIC_END_TEST_CYCLES}</Header>
+    {/if}
 
     <div class="content-align">
         <h2 style="text-align: center">Task</h2>
         {#if testOrTeaching === 'test'}
             <p class="mb-3">{test_intro}</p>
         {/if}
-        {#if testOrTeaching === 'teaching'}
+        {#if testOrTeaching === 'teaching' || testOrTeaching === 'final-test'}
             <p class="mb-3">{teaching_intro}</p>
         {/if}
         <h2 style="text-align: center">Patient Information</h2>
@@ -76,23 +112,12 @@
                     <ListBox
                             active="variant-filled-primary"
                             hover="hover:variant-soft-primary"
-                            display="flex-col"
-                    >
-                        <ListBoxItem bind:group={selected_prediction} name="justify" value={options[0]}
-                        >{options[0]}</ListBoxItem
-                        >
-                        <ListBoxItem bind:group={selected_prediction} name="justify" value={options[1]}
-                        >{options[1]}</ListBoxItem
-                        >
-                        <ListBoxItem bind:group={selected_prediction} name="justify" value={options[2]}
-                        >{options[2]}</ListBoxItem
-                        >
-                        <ListBoxItem bind:group={selected_prediction} name="justify" value={options[3]}
-                        >{options[3]}</ListBoxItem
-                        >
-                        <ListBoxItem bind:group={selected_prediction} name="justify" value={options[4]}
-                        >{options[4]}</ListBoxItem
-                        >
+                            display="flex-col">
+                        {#each options as option, index}
+                            <ListBoxItem bind:group={selected_prediction} name="justify" value={option}
+                                         on:click={() => logPredictionAndDisable(user_id, option, datapoint_count, true_label)}
+                                         {isDisabled}>{option}</ListBoxItem>
+                        {/each}
                     </ListBox>
                 </div>
             </div>
