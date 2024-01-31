7bdb32bf4f7740778804084f16cdc8afc22e1e7b
dimi9595 <dmitry.mindlin@gmail.com>
2024-01-31 15:49:05 +0100

    Trying to make it run on port 3000...

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    server: {
+        host: true,
+        port: 3000
+    }
 });
