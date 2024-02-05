35b8e8bd3f358673608411b60481ba475f3adcc7
Michael Morasch <mmorasch@posteo.de>
2024-02-05 14:29:57 +0100

    wip

--- src/routes/api/+server.ts
+++
@@ -1,8 +0,0 @@
-import type { RequestHandler } from './$types';
-import { setupUserProfile } from '$lib/backend_pg'
-
-export const POST: RequestHandler = async ({request}) => {
-  const body = await request.json()
-  await setupUserProfile(body.username, body.password);
-  return new Response('ok');
-};
