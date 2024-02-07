f6035900b469136a1442349415c1ec880336bd27
dimi9595 <dmitry.mindlin@gmail.com>
2024-02-07 22:59:12 +0100

    Refactor.

--- src/lib/components/Datapoint.svelte
+++ src/lib/components/Datapoint.svelte
@@ -1,21 +1,18 @@
 <script lang="ts">
-  import { writable } from 'svelte/store';
-  export let header: string[];
-  export let body: string[][];
-  export let feature_tooltips: { [key: string]: string };
-  export let feature_units: { [key: string]: string };
-  export let feature_names: TFeatureName[];
-  import TooltipIcon from './TooltipIcon.svelte';
-  import type { TFeatureName } from '$lib/types';
+    export let header: string[];
+    export let body: string[][];
+    export let feature_tooltips: { [key: string]: string };
+    export let feature_units: { [key: string]: string };
+    export let feature_names: TFeatureName[];
+    import TooltipIcon from './TooltipIcon.svelte';
+    import type {TFeatureName} from '$lib/types';
 
-  let rowIndexStore = writable(0);
+    let firstColumnValues = []; // Array to store the values of the first column
 
-  let firstColumnValues = []; // Array to store the values of the first column
-
-  // Populate firstColumnValues with the values of the first column
-  $: {
-    firstColumnValues = body.map(row => row[0]);
-  }
+    // Populate firstColumnValues with the values of the first column
+    $: {
+        firstColumnValues = body.map(row => row[0]);
+    }
 </script>
 
 <div class="table-container w-[90%] mx-auto">
