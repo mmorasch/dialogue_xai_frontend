67a3a92f3325daf3d58d4463b7480e9d0e3275d3
dimi9595 <dmitry.mindlin@gmail.com>
2024-02-08 11:03:15 +0100

    Refactored.

--- src/routes/experiment/+page.ts
+++ src/routes/experiment/+page.ts
@@ -15,7 +15,6 @@ import type {TDatapointResult} from '$lib/types';
 export const load = (async ({url}) => {
     const user_id = url.searchParams.get('user_id');
     const study_group = url.searchParams.get('sg') || undefined;
-    //const study_group: TInteractiveOrStatic = 'static';
     if (user_id === null) throw error(400, 'user_id is required as a query parameter');
 
     // backend returns data that is either questions or report (A/B study design)
