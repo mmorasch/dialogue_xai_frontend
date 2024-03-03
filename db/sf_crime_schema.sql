5e169504c1cce1d059ed6d711b049b8f66bbe1ab
dimi9595 <dmitry.mindlin@gmail.com>
2024-03-03 23:15:20 +0100

    Changed db name and schema.

---
+++ db/sf_crime_schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS users (
+  id text PRIMARY KEY DEFAULT gen_random_uuid(),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  profile jsonb DEFAULT '{}',
+  study_group VARCHAR(255),
+  logs jsonb DEFAULT '{}',
+  questionnaires jsonb DEFAULT '{}',
+  feedback jsonb DEFAULT '{}',
+  completed BOOLEAN DEFAULT FALSE,
+);
+
+CREATE TABLE IF NOT EXISTS events (
+  id SERIAL PRIMARY KEY,
+  user_id text REFERENCES users(id),
+  source VARCHAR(255) NOT NULL,
+  action VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  details jsonb DEFAULT '{}'
+);
+
+CREATE TABLE IF NOT EXISTS user_completed (
+  id SERIAL PRIMARY KEY,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  matrik_num VARCHAR(255) NOT NULL,
+  completed BOOLEAN DEFAULT FALSE NOT NULL
+);
