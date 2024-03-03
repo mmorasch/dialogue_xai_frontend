79b1fa1cfc40125fc51752aeb3f6252a270eddc0
dimi9595 <dmitry.mindlin@gmail.com>
2024-03-03 23:14:54 +0100

    Changed to only incldue matrikelnummer if possible.

--- src/routes/api/setup/+server.ts
+++ src/routes/api/setup/+server.ts
@@ -5,7 +5,9 @@ export const POST: RequestHandler = async ({request}) => {
     const body = await request.json()
     await setupUserProfile(body.user_id, body.profile_data);
     await set_study_group(body.user_id, body.study_group);
-    await createUniParticipant(body.matrikelnummer);
-    await setMatrikNum(body.user_id, body.matrikelnummer);
+    if (body.matrikelnummer !== undefined) {
+        await createUniParticipant(body.matrikelnummer);
+        await setMatrikNum(body.user_id, body.matrikelnummer);
+    }
     return new Response('ok');
 };
