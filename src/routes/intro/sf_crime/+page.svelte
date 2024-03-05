5ef7950c992c55e132cdb667ba0614ab8cde11cd
dimi9595 <dmitry.mindlin@gmail.com>
2024-03-05 15:54:14 +0100

    Added sf_crime intro.

---
+++ src/routes/intro/sf_crime/+page.svelte
@@ -0,0 +1,203 @@
+<script lang="ts">
+    import {goto} from '$app/navigation';
+    import {base} from '$app/paths';
+    import {Step, Stepper} from '@skeletonlabs/skeleton';
+    import {onMount} from 'svelte';
+    import {PUBLIC_TEACH_TEST_CYCLES} from "$env/static/public";
+    import {v4 as uuidv4} from 'uuid';
+    import {PUBLIC_DATASET_NAME} from '$env/static/public';
+
+    let gender: string;
+    let gender_self_identify: string;
+    let age: string;
+    let degree: string;
+    let education_field: string;
+    let education_field_other: string;
+    let english_speaking_level: string;
+    let fam_ml_val: number;
+    let fam_domain_val: number;
+    let consent_given: boolean = false;
+    let pdfPath = `${base}/Diabetes-Consent.pdf`;
+    const user_id = uuidv4();
+    const experiment_start = new Date().toISOString();
+
+    let study_group: any;
+    onMount(() => {
+        fetch(`${base}/api/get_study_group`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            if (response.ok) {
+                response.text().then((text) => {
+                    study_group = text;
+                });
+            } else {
+                console.error('Server responded with non-OK status');
+                study_group = 'static';
+            }
+        }).catch((error) => {
+            console.error('Error:', error);
+            study_group.set('static');
+        });
+    });
+
+    async function onComplete() {
+        let profile_data = {
+            gender: gender,
+            gender_self_identify: gender_self_identify,
+            age: age,
+            degree: degree,
+            education_field: education_field,
+            education_field_other: education_field_other,
+            fam_ml_val: fam_ml_val,
+            fam_domain_val: fam_domain_val,
+            english_speaking_level: english_speaking_level,
+            experiment_start: experiment_start
+        };
+
+        fetch(`${base}/api/setup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                user_id: user_id,
+                profile_data: profile_data,
+                study_group: study_group,
+            })
+        });
+        goto(`${base}/experiment?user_id=${user_id}&sg=${study_group}`);
+    }
+
+    let study_group_interactive_text =
+        'Get familiar with the ML model\'s prediction in an <b>interactive chatbot</b>.';
+
+    let study_group_static_text =
+        'Get familiar with the ML model\'s prediction in an <b>explanation report</b>.';
+</script>
+
+<div class="col-start-2 col-end-2 space-y-4 p-2 sm:p-2 md:space-y-6">
+    <h1 class="text text-4xl">Welcome!</h1>
+    <!-- https://www.skeleton.dev/components/steppers -->
+    <Stepper buttonCompleteLabel="Start Experiment" on:complete={onComplete}>
+        <Step>
+            <p>
+                This is a study on <b>understanding the decision process of Artificial Intelligence (AI) models</b>
+                which takes about 15-20 minutes. It is designed as part of a large research project
+                that seeks to improve understanding between AI Systems (i.e. Machine learning models) and humans.<br/>
+            </p>
+
+            <p class="m my-12">
+                Thank you for your participation! <br/><br/>
+
+                Sincerely, <br/><br/>
+
+                <b>Dimitry Mindlin</b>, Bielefeld University
+            </p>
+        </Step>
+        <Step>
+            <h2 class="text-2xl">Your Experiment ID</h2>
+            <p>
+                Please <b>keep your experiment ID</b> for future reference. <br>
+                You will need it to delete your data from the study if you wish.
+                <br><br>
+                {user_id}
+                <br><br>
+                We value your privacy; so, your with your experiment data will not be shared with any third party, and
+                we cannot identify you personally from your experiment data.
+                This ID is essential for us to identify and delete your data upon request.
+            </p>
+        </Step>
+        <Step>
+            <h2 class="text-2xl">
+                Experiment topic: Understanding AI predictions in predictive policing
+            </h2>
+            <p>
+                In this experiment, you will <b>discover how machine learning models make decisions</b> when predicting
+                the
+                likelihood of different crime levels for different discricts in San Francisco.
+                <br/>
+            </p>
+        </Step>
+        <Step>
+            <h2 class="text-2xl">
+                Concrete Example
+            </h2>
+            <p>For example, given a district with the following information (called attributes):</p>
+            <ul class="list">
+                <li><span class="flex-auto">- Avg Temp: 22 °C</span></li>
+                <li><span class="flex-auto">- Day Of Week: Monday</span></li>
+                <li><span class="flex-auto">- Month: 2</span></li>
+                <li><span class="flex-auto">- Month Period: MiddleOfMonth</span></li>
+                <li><span class="flex-auto">- NFl Game Day: True</span></li>
+                <li><span class="flex-auto">- Pd District: PARK</span></li>
+            </ul>
+            <p>
+                The model would give a <b>prediction</b> whether this district has a <b>low crime</b>, <b>medium
+                crime</b> or <b>high crime</b> level.
+            </p>
+        </Step>
+        <Step>
+            <h2 class="text-2xl">Experiment Introduction - How will the model predict the crime level of different
+                districts?</h2>
+            <div class="container">
+                <h1>The experiment is structured as follows:</h1>
+                <ol>
+                    <li><b style="color: green;">Learning Phase:</b> Given one district's information at a time,
+                        <b>guess the Machine Learning model's decision</b>.
+                    </li>
+                    {#if study_group === 'interactive'}
+                        <li>{@html study_group_interactive_text}</li>
+                    {:else}
+                        <li>{@html study_group_static_text}</li>
+                    {/if}
+                    <li>When you feel that you understand the reason for the models decision, proceed by clicking <b>Proceed</b>.
+                    </li>
+                    <li><b style="color: purple;">Testing Phase:</b> Guess the model prediction for a new district based
+                        on your
+                        understanding. For this <b>new district, you will not
+                            receive the model's prediction or explanations</b>, since it is a test of your
+                        understanding.
+                    </li>
+                    <li>Complete this process for a total of <b>{PUBLIC_TEACH_TEST_CYCLES}
+                        learning-testing district pairs </b>.
+                    </li>
+                    <li>Answer a few <b>questions about your model understanding</b> at the end.</li>
+                </ol>
+            </div>
+        </Step>
+        <Step>
+            <h2 class="text-2xl">General Information</h2>
+            <p class="note">Attention: The goal isn't to be right about whether a district is high crime or not. It's
+                all
+                about estimating what the model would predict based on the explanations you see.</p>
+            <br>
+            <p>
+                Do not use the <b>browser's back button</b> during the experiment. This will cause the <b>experiment to
+                restart</b>.
+                <br>
+                <br>
+            </p>
+        </Step>
+    </Stepper>
+</div>
+
+
+<style>
+
+    ol {
+        margin: 20px 0;
+        list-style-type: decimal;
+    }
+
+    li {
+        margin: 10px 0;
+    }
+
+    .note {
+        font-weight: bold;
+    }
+
+</style>
