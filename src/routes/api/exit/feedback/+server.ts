65790e66d59b1be6052a2008c449f92abac92a51
dimi9595 <dmitry.mindlin@gmail.com>
2024-03-05 17:01:02 +0100

    hacky, removed matrikel_num.

--- src/routes/api/exit/feedback/+server.ts
+++ src/routes/api/exit/feedback/+server.ts
@@ -5,8 +5,9 @@ export const POST: RequestHandler = async ({request}) => {
     const body = await request.json()
     await logFinalFeedback(body.user_id, body.feedback);
     await logCompleted(body.user_id);
-    const matrikelnummer = await getMatrikNum(body.user_id);
-    await assignFinishedUniParticipant(matrikelnummer);
-    await deleteMatrikNum(body.user_id);
+    // TODO: Make conditioned on dataset
+    //const matrikelnummer = await getMatrikNum(body.user_id);
+    //await assignFinishedUniParticipant(matrikelnummer);
+    //await deleteMatrikNum(body.user_id);
     return new Response('ok');
 };
