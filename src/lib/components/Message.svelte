d844f5ae5b41cec2a9c0926383b0e6562a7f9ab4
dimi9595 <dmitry.mindlin@gmail.com>
2024-03-11 13:14:26 +0100

    Introduced followup questions in message.

--- src/lib/components/Message.svelte
+++ src/lib/components/Message.svelte
@@ -2,7 +2,6 @@
     import LikeButton from './LikeButton.svelte';
     import DislikeButton from './DislikeButton.svelte';
     import {createEventDispatcher} from 'svelte';
-    import CommentButton from './CommentButton.svelte';
     import type {TChatMessage} from '$lib/types';
     import {fade} from 'svelte/transition';
 
@@ -10,22 +9,40 @@
 
     const dispatch = createEventDispatcher();
 
+    // Function to forward event
     function forwardEvent(event) {
         dispatch('feedbackButtonClick', event.detail);
+    }
+
+    // Function to handle button click for questions
+    function buttonOnClick(questionId: number) {
+        console.log(`Question with ID ${questionId} clicked`);
+        dispatch('questionClick', {questionId});
     }
 </script>
 
 <div class="flex items-end mb-2.5 {message.isUser ? 'right-msg' : 'left-msg'}">
     <div class="msg-bubble max-w-md p-2.5 rounded-2xl">
-
         {@html message.text}
-        {#if !message.isUser}
-            {#if message.feedback}
-                <br/>
-                <span class="float-right flex flex-wrap" in:fade={{delay: 1000, duration: 250}}>
-					<LikeButton {message} on:feedbackButtonClick={forwardEvent}/>
-                    <DislikeButton {message} on:feedbackButtonClick={forwardEvent}/>
-				</span>
+        {#if !message.isUser && message.feedback}
+            <br/>
+            <span class="float-right flex flex-wrap" in:fade={{delay: 1000, duration: 250}}>
+                <LikeButton {message} on:feedbackButtonClick={forwardEvent}/>
+                <DislikeButton {message} on:feedbackButtonClick={forwardEvent}/>
+            </span>
+            <!-- Check if the followup array is not empty -->
+            {#if message.followup && message.followup.length > 0}
+                <div class="follow-up-questions mt-4">
+                    <p>Suggested Follow-up Questions:</p>
+                    {#each message.followup as question}
+                        <button
+                                data-value={question.id}
+                                type="button"
+                                class="btn variant-ghost-primary"
+                                style="font-size: 0.75rem;"
+                                on:click={() => buttonOnClick(question.id)}>{question.question}</button>
+                    {/each}
+                </div>
             {/if}
         {/if}
     </div>
@@ -42,5 +59,20 @@
 
     .right-msg .msg-bubble {
         @apply rounded-br-none variant-ghost-tertiary;
+    }
+
+    .follow-up-questions p {
+        @apply font-semibold;
+    }
+
+    .msg-bubble .btn, .btn {
+        @apply m-1 p-2 bg-transparent border border-solid border-gray-300 rounded cursor-pointer text-left block w-full;
+        white-space: normal; /* Ensure text wraps */
+        overflow: hidden; /* Keep the content within the button */
+        max-width: 100%; /* Ensure button does not exceed its container */
+        transition: background-color 0.3s ease;
+        &:hover {
+            @apply bg-gray-200;
+        }
     }
 </style>
