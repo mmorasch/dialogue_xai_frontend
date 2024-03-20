04398f859db4025e50396f98fd4a844021deca06
dimi9595 <dmitry.mindlin@gmail.com>
2024-03-20 14:23:57 +0100

    Created SubmitButton component.

---
+++ src/lib/components/SubmitButton.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let next;
+    export let label = "Proceed"; // Default label if none is provided
+</script>
+
+<input type="submit" value={label} class="submit-button" on:click|preventDefault={next} />
+
+<style lang="postcss">
+    .submit-button {
+        @apply bg-black text-white rounded-lg cursor-pointer mx-0 my-5 px-5 py-3.5 border-none;
+        position: absolute;
+        bottom: 0;
+        left: 15%;
+        width: 70%;
+    }
+</style>
