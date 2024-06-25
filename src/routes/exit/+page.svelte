7a04fad007a8198e5c76720e1e4992b7b03601df
dimi9595 <dmitry.mindlin@gmail.com>
2024-06-25 10:46:35 +0200

    Introduced UnderstandingQuestionnaire.svelte

--- src/routes/exit/+page.svelte
+++ src/routes/exit/+page.svelte
@@ -1,17 +1,30 @@
 <script>
     import ExitQuestionnaire from './ExitQuestionnaire.svelte';
+    import UnderstandingQuestionnaire from './UnderstandingQuestionnaire.svelte';
+
     import {userId, studyGroup} from "$lib/shared.ts";
 
     let user_id;
     let study_group;
+    let understandingCompleted = false;
+
     if (typeof window !== 'undefined') {
         user_id = userId.get();
         study_group = studyGroup.get();
+    }
+
+    // Function to handle the completion of the UnderstandingQuestionnaire
+    function handleUnderstandingComplete() {
+        understandingCompleted = true;
     }
 </script>
 
 <div class="col-span-3 space-y-4 p-6 sm:p-8 md:space-y-6 w-3/4 mx-auto">
     {#if user_id}
-        <ExitQuestionnaire {user_id} {studyGroup}/>
+        {#if !understandingCompleted}
+            <UnderstandingQuestionnaire {user_id} {studyGroup} on:complete={handleUnderstandingComplete}/>
+        {:else}
+            <ExitQuestionnaire {user_id} {studyGroup}/>
+        {/if}
     {/if}
 </div>
---
+++ src/routes/exit/UnderstandingQuestionnaire.svelte
@@ -0,0 +1,140 @@
+<script lang="ts">
+    import {base} from '$app/paths';
+    import {Step, Stepper} from '@skeletonlabs/skeleton';
+    import {createEventDispatcher} from "svelte";
+    import SubmitButton from "$lib/components/SubmitButton.svelte";
+
+    export let user_id: string;
+
+    const dispatch = createEventDispatcher();
+
+    let most_imp_val = "";
+    let lest_imp_val = "";
+    let decision_rules_text = "";
+
+    let study_questions = [
+        "Which attribute was, on average, the most important for the model’s decision?",
+        "Which attribute was, on average, the least important for the model’s decision?",
+        "What rules do you think the model uses to differentiate between individuals earning over 50k and under 50k?"
+    ];
+
+    // Validate form before submitting
+    function validateForm() {
+        if (!most_imp_val || !lest_imp_val || !decision_rules_text.trim()) {
+            alert("Please complete all questions before submitting.");
+            return false;
+        }
+        return true;
+    }
+
+
+    // Save Answers to Database
+    async function onComplete() {
+        if (!validateForm()) return;
+
+        // Save the answers when the user completes the questionnaire
+        await Promise.all([
+            fetch(`${base}/api/exit/questionnaire`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    user_id: user_id,
+                    questions: study_questions,
+                    answers: [most_imp_val, lest_imp_val, decision_rules_text],
+                    correct_answers: ["maritalStatus", "workLifeBalance", "..."],
+                    questionnaire_name: 'understanding'
+                })
+            }),
+        ]);
+        dispatch('complete');
+    }
+</script>
+
+<div>
+    <!-- https://www.skeleton.dev/components/steppers -->
+    <Stepper stepTerm="Question">
+        <Step>
+            <h1 class="center-text text-xl">Almost done! <br> Here are <b>three final questions</b> about your <b>model
+                understanding</b>.</h1>
+            <div class="flex-container">
+                <label class="label text-center flex-item">
+                    <span>Which attribute was, on average, the <b>most important</b> for the model’s decision?</span>
+                    <select bind:value={most_imp_val} class="select py-1">
+                        <option value="" selected>- Select -</option>
+                        <option value="age">Age</option>
+                        <option value="maritalStatus">Marital Status</option>
+                        <option value="workExperience">Work Experience</option>
+                        <option value="gender">Gender</option>
+                        <option value="workLifeBalance">Work Life Balance</option>
+                    </select>
+                </label>
+                <label class="label text-center flex-item">
+                    <span>Which attribute was, on average, the <b>least important</b> for the model’s decision?</span>
+                    <select bind:value={lest_imp_val} class="select py-1">
+                        <option value="" selected>- Select -</option>
+                        <option value="age">Age</option>
+                        <option value="investmentOutcome">Investment Outcome</option>
+                        <option value="workLifeBalance">Work Life Balance</option>
+                        <option value="gender">Gender</option>
+                        <option value="workExperience">Work Experience</option>
+                    </select>
+                </label>
+            </div>
+            <h1 class="center-text">What <b>rules</b> do you think the model uses to <b>differentiate between
+                individuals</b>
+                earning over 50k and under 50k?</h1>
+            <textarea bind:value={decision_rules_text} class="textarea-full-width"
+                      placeholder="I found that the model uses... I think the model distinguishes by..."></textarea>
+            <div class="button-container">
+                <SubmitButton next={onComplete} label="Submit" customStyle="padding: 15px; margin-top: 10px;"/>
+            </div>
+        </Step>
+    </Stepper>
+</div>
+
+<style>
+
+    .button-container {
+        display: flex;
+        justify-content: flex-end;
+        margin-top: 1em;
+    }
+
+    .center-text {
+        display: block;
+        text-align: center;
+    }
+
+    .flex-container {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 1em;
+    }
+
+    .flex-item {
+        flex: 1;
+        margin-right: 1em;
+    }
+
+    .flex-item:last-child {
+        margin-right: 0;
+    }
+
+    .select {
+        height: 2.5em; /* Adjust the height as needed */
+    }
+
+    .textarea-full-width {
+        width: 100%;
+        height: 6em; /* Adjust the height as needed */
+        resize: vertical; /* Allow users to resize if needed */
+    }
+
+    .input-area button {
+        @apply rounded-lg cursor-pointer px-8 py-2.5 border-2;
+        margin-right: 10px;
+    }
+
+</style>
